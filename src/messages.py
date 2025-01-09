d74061934e6189767c9a01d3c3bc07cd5df6a232
agomez <a@a>
2025-01-09 20:39:18 +0100

    Page/slide counter messages

--- src/messages.py
+++ src/messages.py
@@ -1,13 +1,14 @@
 # General Messages
 CHECK_WITH_PARSER = "Check well-formedness"
-SELECT_DIRECTORY = "Select Directory Containing XML Files"
+SELECT_DIRECTORY = "Select Directory Containing Files"
 SELECT_LOG_DIRECTORY = "Select Directory to Save Log"
 NO_XML_FILES_FOUND = "No XML files found in the selected directory."
-LOG_SAVED = "Results saved to log file: "
+LOG_SAVED = "Results saved to log file: '{0}'"
 LOG_ERROR = "Failed to save log: "
-LOG_HEADER = "Validation Results for XML Files in Directory: "
+LOG_HEADER = "Results for Files in Directory: "
 LOG_TIMESTAMP = "Log generated on: {0}"
 LOG_SEPARATOR = "=" * 50
+LOG_FILE_SEPARATOR = "-" * 50
 VALIDATED_FILE = "Validated File: {0}"
 RESULT_SUCCESS = "Result: Success"
 RESULT_FAILURE = "Result: Failure"
@@ -17,7 +18,7 @@ VALID_XML = "The XML file '{0}' is well-formed."
 INVALID_XML = "Error: The XML file '{0}' is not well-formed."
 NO_LOG_FILE_SELECTED = "No log file selected. Results not saved."
 NO_XML_FILES = "No XML files found in the selected directory."
-LOG_FILE_ERROR = "Error writing to log file: "
+LOG_FILE_ERROR = "Error writing to log file: '{0}'"
 GUI_TITLE = "XML Validator with Log"  # Title for the GUI window
 
 # Error Messages
@@ -46,6 +47,7 @@ VALIDATION_ERROR = "Validation failed for the XML file '{0}' using the DTD/STD."
 VALIDATION_SUCCESS = "Validation succeeded for the XML file '{0}' using the DTD/STD."
 VALIDATION_FAILED = "Validation for the XML file '{0}' has failed."
 FILE_PARSE_ERROR = "The following error has ocurred while parsing file '{0}': {1}"
+UNSUPPORTED_FILE_TYPE = "'{0}' file type is unsupported."
 
 # New Messages for Refactored GUI
 EXIT_BUTTON = "Exit"
@@ -55,3 +57,24 @@ FILE_MENU = "File"
 XML_TASKS_MENU = "XML Tasks"
 CHECK_WELL_FORMEDNESS = "Check well-formedness"
 VALIDATE_WITH_DTD_STD_MENU = "Validate with DTD/STD"
+
+# Slide/page counter messages
+PAGE_SLIDE_COUNTER = "Page/slide counter"
+PAGE_COUNT_RESULT = "File '{0}': {1} page(s)"
+SLIDE_COUNT_RESULT = "File '{0}': {1} slide(s)"
+PDF_PAGE_COUNT_RESULT = "File '{0}': {1} page(s)"
+NO_MS365_FILES_FOUND = "No supported Microsoft 365 files found in the directory."
+INVALID_DIRECTORY = "Invalid directory: {}"
+# Message for total Word pages count
+TOTAL_WORD_PAGES = "Total Word pages: {0}"
+# Message for total PowerPoint slides count
+TOTAL_PPT_SLIDES = "Total PowerPoint slides: {0}"
+# Message for total PDF pages count
+TOTAL_PDF_PAGES = "Total PDF pages: {0}"
+
+# Message for DTP time
+WORD_DTP_TIME = "DTP Time for Word Documents (hours): {:.2f}"
+PPT_DTP_TIME = "DTP Time for PowerPoint Presentations (hours): {:.2f}"
+PDF_DTP_TIME = "DTP Time for PDF Files (hours): {:.2f}"
+
+TOTAL_DTP_TIME = "Total DTP Time (hours): {:.2f}"
